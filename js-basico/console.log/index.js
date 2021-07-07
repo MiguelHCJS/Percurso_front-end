c874b9ad39c83dea6bc8e06c0bca82855a4f4b0c
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-06 23:32:30 -0300

    Uso de 'console.log'

--- js-basico/console.log/index.js
+++ js-basico/console.log/index.js
@@ -1,1 +1,9 @@
+// É demonstrado o que for inserido entre ()
+
 console.log('Olá mundo!');
+
+// texto = string
+// com possibilidade para usar - aspas simples (''), dupla ("") ou (``)
+// exemplo de uso para mesclar entre si
+
+console.log('Me chamo "Miguel", estou aprendendo a programar com JS.')
