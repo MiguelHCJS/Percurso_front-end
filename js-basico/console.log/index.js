38d3f5759565583b00ecd303b74c8175a5d50de4
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-06 23:54:35 -0300

    Comentários: linha e bloco

--- js-basico/console.log/index.js
+++ js-basico/console.log/index.js
@@ -6,4 +6,11 @@ console.log('Olá mundo!');
 // com possibilidade para usar - aspas simples (''), dupla ("") ou (``)
 // exemplo de uso para mesclar entre si
 
-console.log('Me chamo "Miguel", estou aprendendo a programar com JS.')
+console.log('Me chamo "Miguel", estou aprendendo a programar com JS.');
+
+/* 
+
+Comentario em linha com (// ...)
+E comentário em bloco (linha 11 e linha 16)
+
+ */
