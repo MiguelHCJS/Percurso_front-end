ff2606e537a948d41fd5fa549426861dd983b809
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-09 12:54:15 -0300

    Padronização de pastas e inserção de mais conteúdo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Curso_JSeTS
  Curso para JS, com DB, API e etc...
+
+A estrutura das pastas numeradas é para mostrar o caminho no qual estou seguindo para construir o conhecimento teórico, assim como os 'index' servem para demonstrar a parte prática.
---
+++ js-basico/02_variáveis/index01e02.js
@@ -0,0 +1,23 @@
+/*
+
+Melhor uso da variável criada como 'const' é aquela que não haverá possibilidade de mudanças.
+
+Para a variável criada como 'let' é possível sua mudança de valor
+
+*/
+
+/*
+
+APLICAÇÃO DE VARIÁVEIS
+
+Me chamo Miguel e sou de Alagoas, tenho 25 anos e moro há alguns anos em São Paulo, sou formado em Administração e estudo atualmente o desenvolvimento web.
+
+*/
+
+const nome = 'Miguel';
+const estado = 'Alagoas';
+let idade = 25;
+let cidade = 'São Paulo';
+const formacao = 'Administração';
+
+console.log(`Me chamo ${nome} e sou de ${estado}, tenho ${idade} anos e moro há alguns anos em ${cidade}, sou formado em ${formacao} e estudo atualmente o desenvolvimento web.`);
