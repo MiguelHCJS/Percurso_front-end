4506d13a9f04dff4be2cb3aa509d2b44df8249bc
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-08 23:13:38 -0300

    variavel const

---
+++ js-basico/02_variáveis/const/index.js
@@ -0,0 +1,12 @@
+/*
+
+A variável 'const' precisa receber um valor no momento que for declarada
+Não é alterada posteriormente, mas pode ser atribuida a outra 'const'
+
+*/
+
+const nome1 = 'Miguel';
+const nome2 = 'Tenorio';
+const nomeCompleto = nome1 +' '+ nome2;
+
+console.log(nomeCompleto);
