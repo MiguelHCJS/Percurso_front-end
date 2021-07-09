12f939bd94719dedc0206dc3f844151007b1724b
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-09 14:27:04 -0300

    Tipos de variáveis

---
+++ 01_js-basico/02_variáveis/04_tipoDedados.js
@@ -0,0 +1,13 @@
+const nome = "Miguel"; // string
+const nome1 = 'Miguel' // string
+const nome2 = `Miguel`; // string
+const num = 1; // number
+const num1 = 1.1; // number
+let numeAluno; // undefined - pois não é passado nenhum valor e não aponta em nenhum lugar em memória.
+let valor = null; // nulo - declaro com objetivo dela não ter valor, por alguma necessidade específica.
+const aprovado = true; // boolean - lógico
+const reprovado = false; // boolean - lógico
+
+/* Para identificar o tipo de dado de uma variável, usar typeof */
+
+console.log(typeof(nome)); // Console mostrará string
