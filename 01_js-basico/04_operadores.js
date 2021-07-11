6c5cb3def92b111992b4e97cda688144a0b44e9c
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-11 15:20:52 -0300

    Operadores aritiméticos

---
+++ 01_js-basico/04_operadores.js
@@ -0,0 +1,21 @@
+/*
+
++ soma / concatenação
+- subtração
+* multiplicação
+** potenciação
+/ divisão
+% resto da divisão
+
+*/
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(num1 + num2); // soma
+console.log(num1 + ' e ' + num2); // concatenação
+console.log(num1 - num2); // subtração
+console.log(num1 * num2); // multiplicação
+console.log(num1 ** num2); // potenciação
+console.log(num1 / num2); // divisão
+console.log(num1 % num2); // resto de divisão
