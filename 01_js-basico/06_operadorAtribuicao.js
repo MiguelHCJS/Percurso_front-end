0d2b58c272f82d6178bff1ddd3abd7884272cbec
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-11 22:24:51 -0300

    Sobre operadores de atribuição

---
+++ 01_js-basico/06_operadorAtribuicao.js
@@ -0,0 +1,32 @@
+/*
+
+Uma forma eficiente para atribuir a um valor e já aplicar um dos operadores:
+
+ex:
+
+1 + 1 = 2
+
+variável + valor = variável
+
+*/
+
+let num = 1;
+let num1 = 1;
+let num2 = 1;
+let num3 = 1;
+
+num += 1;
+num1 -= 1;
+num2 *= 2;
+num3 /= 2;
+
+console.log(`A soma 1 + 1 = ${num}, a subtração 1 - 1 = ${num1}, a multiplicação 1 * 2 = ${num2} e divisão 1 / 2 = ${num3}.`);
+
+/*
+
+Estes operadores de atribuição funciona para os outros também:
+
+** potenciação
+% resto de divisão
+
+*/
