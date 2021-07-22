7d4ed9df487d75c2ad0dc6410521e4c87172b74a
Miguel Tenório <miguel.h.c.j.s@gmail.com>
2021-07-22 00:05:42 -0300

    Complemento de exercício

--- 03_javascript/01_js-basico/15_ex-array-funcao-objeto/js/js.js
+++ 03_javascript/01_js-basico/15_ex-array-funcao-objeto/js/js.js
@@ -1,9 +1,23 @@
+// função de contexto global
 function meuEscopo() {
+    // selecionando o formulário e a área de resultado
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
+    // array criado para receber objetos e indexar
     const pessoas = [];
 
+    /*
+    A FUNÇÃO SEGUINTE FAZ:
+    
+    1° EVITA A PÁGINA SER RECARREGADA TODA VEZ QUE PRESSIONADO O BOTÃO ENVIAR(SUBMIT)
+
+    2° RECEBE OS VALORES ADICIONADOS PELO USUÁRIO NOS CAMPOS DE INPUT DO FORMULÁRIO
+
+    3° COM O MÉTODO PUSH, ACRESCENTA NO ARRAY ANTERIORMENTE CRIADO OS VALORES DIGITADOS PELO USUÁRIO
+
+    4° DEMONSTRA OS VALORES NO CONSOLE E OS INSERE NO HTML
+    */
     function recebe (evento) {
         evento.preventDefault();
 
@@ -24,7 +38,8 @@ function meuEscopo() {
         resultado.innerHTML += `<p>Nome: ${nome.value}<br> Sobrenome: ${sobrenome.value}<br> Peso: ${peso.value}<br> Altura: ${altura.value}</p>`;
     };
 
+    // QUANDO O EVENTO SUBMIT É SELECIONADO, EXECUTA A FUNÇÃO ANTERIORMENTE CRIADA
     form.addEventListener('submit', recebe);
-}
+};
 
 meuEscopo();
