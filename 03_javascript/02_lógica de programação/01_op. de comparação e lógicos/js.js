4be704e8bf6c5bbc932d0a449ce8606342960a4f
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2021-11-15 16:31:08 -0300

    Atualização

---
+++ 03_javascript/02_lógica de programação/01_op. de comparação e lógicos/js.js
@@ -0,0 +1,34 @@
+/* 
+
+- OPERADORES DE COMPRAÇÃO
+
+> maior que
+
+>= maior que ou igual a
+
+< menor que
+
+<= menor que ou igual a
+
+== igualdade (valor)
+
+=== igualdade estrita (valor e tipo)
+
+!= diferente (valor)
+
+!== diferente estrito (valor e tipo)
+
+
+- OPERADORES LÓGICOS
+
+&& > and > "e"
+
+Em uma operação, retorna verdadeiro se os dois valores corresponderem a sentença.
+
+|| > or > ou
+
+Em uma operação, apenas um está saldável
+
+! > not > não
+
+*/
