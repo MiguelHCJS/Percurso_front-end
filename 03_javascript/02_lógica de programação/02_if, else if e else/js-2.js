ecc6bad1d5703324504a95263c3b4f7da79bcd04
Miguel Tenório <69335370+MiguelHCJS@users.noreply.github.com>
2021-11-22 23:46:26 -0300

    If e else

---
+++ 03_javascript/02_lógica de programação/02_if, else if e else/js-2.js
@@ -0,0 +1,21 @@
+/*
+
+Estrutura if
+
+if (condição) {
+    ação que será realizada se condição verdadeira
+} else {
+    ação que será realizada se a primeira condição não for verdadeira
+}
+
+*/
+
+const hora = 19;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia!');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde!');
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite!');
+};
---
+++ 03_javascript/02_lógica de programação/02_if, else if e else/js.js
@@ -0,0 +1,27 @@
+/*
+
+Estrutura if
+
+if (condição) {
+    ação que será realizada se condição verdadeira
+} else {
+    ação que será realizada se a primeira condição não for verdadeira
+}
+
+*/
+
+const hora = 12;
+
+// hora menor ou igual a
+if (hora <= 12) {
+    console.log('Bom dia');
+} else {
+    console.log('Olá, bem vindo!');
+};
+
+// hora maior que 13
+if (hora > 13) {
+    console.log('Bom dia');
+} else {
+    console.log('Olá, bem vindo!');
+};
